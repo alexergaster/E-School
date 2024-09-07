ce068ae830157123b59c71a3aa747ca70423d98f
alexergaster <alexanderergaster@gmail.com>
2024-09-07 15:18:08 +0300

    added SEO

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('images/misc/logo.png') }}" type="image/x-icon">
+    <meta name="robots" content="noindex, nofollow">
     <title>E-School ADMIN</title>
 
     <!-- Google Font: Source Sans Pro -->
--- resources/views/parent/show.blade.php
+++ resources/views/parent/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
 
+@section('robots', 'noindex, nofollow')
+
 @section('content')
 
     <div class="main__parrent">
